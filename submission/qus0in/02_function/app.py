546e92968de1aed6bf9073363f7faeb7c37c1cfa
Young-in Byeon <50694786+qus0in@users.noreply.github.com>
2023-03-06 17:09:47 +0900

    headline

--- submission/qus0in/02_function/app.py
+++ submission/qus0in/02_function/app.py
@@ -1,4 +1,9 @@
 # streamlit 라이브러리 호출
+# 상대경로 복사 : submission/qus0in/02_function/app.py
 import streamlit as st
 
-st.write("함수를 응용해서 페이지 만들어보기")
+# st.write("함수를 응용해서 페이지 만들어보기")
+
+# 로또 번호 추천 프로그램
+
+st.write("# 일주일의 행복 로또")
