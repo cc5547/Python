d4b851ea87677b7ae97d91e295d3985cd4f78e14
고병연 <59509808+mastgm0817@users.noreply.github.com>
2023-03-06 17:48:20 +0900

    sidebar

--- submission/mastgm0817/02_second_webapp/app.py
+++ submission/mastgm0817/02_second_webapp/app.py
@@ -5,3 +5,17 @@ import streamlit as st
 st.write(
     "두번째 시작"
 )
+
+
+# Using object notation
+add_selectbox = st.sidebar.selectbox(
+    "How would you like to be contacted?",
+    ("Email", "Home phone", "Mobile phone")
+)
+
+# Using "with" notation
+with st.sidebar:
+    add_radio = st.radio(
+        "Choose a shipping method",
+        ("Standard (5-15 days)", "Express (2-5 days)")
+    )
