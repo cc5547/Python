94982116b3365265d90df78f7a04a5f8558dd495
고병연 <59509808+mastgm0817@users.noreply.github.com>
2023-03-06 18:00:01 +0900

    guess_number

--- submission/mastgm0817/02_second_webapp/app.py
+++ submission/mastgm0817/02_second_webapp/app.py
@@ -8,7 +8,7 @@ st.write(
 
 guess = st.slider('번호를 고르세요', min_value=1, max_value=10, step=1)
 print(f" 당신이 선택한 번호는 {guess} 입니다.")
-
+st.write(f" 당신이 선택한 번호는 {guess} 입니다.")
 prize = st.slider('당첨 상금을 지정해주세요', min_value=10000, max_value=10000000, step=5000)
 print(f" 당첨금액은  {prize} 입니다.")
 
