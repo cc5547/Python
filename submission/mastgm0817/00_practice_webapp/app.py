22d1bfa1e950c1dc78e4c84d43f701ac1f5e7fa2
고병연 <59509808+mastgm0817@users.noreply.github.com>
2023-03-07 11:23:55 +0900

    커밋

--- submission/mastgm0817/00_practice_webapp/app.py
+++ submission/mastgm0817/00_practice_webapp/app.py
@@ -2,8 +2,5 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-chart_data = pd.DataFrame(
-   "submission/mastgm0817/00_practice_webapp/국비지원반.CSV",
-    columns=['이름', '성별', '직업'])
-
-st.line_chart(chart_data)
+df = pd.read_csv("submission/mastgm0817/00_practice_webapp/국비지원반.CSV")
+df.plot()
