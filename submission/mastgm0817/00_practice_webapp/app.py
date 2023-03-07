65f797c32cdca1b67bb50bdc6c76ae4c83eb8be7
고병연 <59509808+mastgm0817@users.noreply.github.com>
2023-03-07 11:21:46 +0900

    파이

--- submission/mastgm0817/00_practice_webapp/app.py
+++ submission/mastgm0817/00_practice_webapp/app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 
 chart_data = pd.DataFrame(
-   "국비지원반.csv",
+   "submission/mastgm0817/00_practice_webapp/국비지원반.CSV",
     columns=['이름', '성별', '직업'])
 
 st.line_chart(chart_data)
