43f780a1b649ac95a5a87ca1e6ce98d81aae9109
고병연 <59509808+mastgm0817@users.noreply.github.com>
2023-03-07 11:35:22 +0900

    button

--- submission/mastgm0817/00_practice_webapp/app.py
+++ submission/mastgm0817/00_practice_webapp/app.py
@@ -3,8 +3,13 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import io
 
-uploaded_file = st.file_uploader("Choose a file")
-if uploaded_file is not None:
-    data = uploaded_file.read().decode("utf-8")
-    df = pd.read_csv(io.StringIO(data))
-    st.dataframe(df)
+
+def upload_csv():        
+    uploaded_file = st.file_uploader("Choose a file")
+    if uploaded_file is not None:
+        data = uploaded_file.read().decode("utf-8")
+        df = pd.read_csv(io.StringIO(data))
+        st.dataframe(df)
+
+pushed = st.button("번호 추첨", on_click=upload_csv)
+
