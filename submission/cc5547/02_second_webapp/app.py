d3d0943749d0a3a21ebeeea6fa24ba5e813f55a7
cc5547 <126434132+cc5547@users.noreply.github.com>
2023-03-15 16:40:10 +0900

    go

--- submission/cc5547/02_second_webapp/app.py
+++ submission/cc5547/02_second_webapp/app.py
@@ -1,14 +1,15 @@
 import streamlit as st
-
 st.set_page_config(layout="wide")
-
 
 # 컬럼 생성 : 공간을 8:2 으로 분할하여 col1, col2 이름을 가진 컬럼을 생성합니다.  
 # col1, col2 = st.columns([8, 2])
+ # with col2 :
+  #   # column 2 에 담을 내용
+  #   st.title('컬럼2')
+  #   st.checkbox('컬럼2의 체크박스')
 
 # 단일 컬럼 생성
 col = st.columns(1)
-
 
 # 탭 생성 : 첫번째 탭의 이름은 Tab A 로, Tab B로 표시합니다. 
 tab1, tab2= st.tabs(['Tab_1' , 'Tab_2'])
@@ -16,25 +17,23 @@ tab1, tab2= st.tabs(['Tab_1' , 'Tab_2'])
 # 사이드바 생성 : 사이드 바를 s_bar 로 생성.
 s_bar = st.sidebar
 
+ # 사진 URL
+picture_URL = "https://i.imgur.com/TT1ordb.png"
 
-picture_URL = "https://i.imgur.com/D7uu8FN.jpg" # 잠시 전역변수
-
-def main():
-
+def side_bar():
   # 사이드바
   s_bar.title('여기가 사이드바 입니다.')
   s_bar.checkbox('체크박스에 표시될 문구입니다.')
 
+
+def main():
+  def side_bar()
+
   with col[0] :
     # column 에 담을 내용
-    st.title('# 지도')
+    st.title('#지도')
     st.image(picture_URL)
 
-  # with col2 :
-  #   # column 2 에 담을 내용
-  #   st.title('컬럼2')
-  #   st.checkbox('컬럼2의 체크박스')
-    
   with tab1 :
     #tab A 를 누르면 표시될 내용
     st.write('안녕하다')
