b77a3af4daa2fbc5d81e629d0771b6c95bb02921
cc5547 <126434132+cc5547@users.noreply.github.com>
2023-03-16 17:29:34 +0900

    go

--- submission/cc5547/02_second_webapp/app.py
+++ submission/cc5547/02_second_webapp/app.py
@@ -56,7 +56,7 @@ def main():
     # column 에 담을 내용
     st.title(':smile: 시험장소를 안내해드립니다 :smile:')
     # 데이터프레임 출력 및 사이즈 조절
-    st.dataframe(result.style.set_table_styles([{'selector': 'th', 'props': [('max-width', '150px')]}])) # table로도 작성해볼것, width=1000, height=500
+    st.dataframe(result.style.set_table_styles([{'selector': 'th', 'props': [('max-width', '300px')]}])) # table로도 작성해볼것, width=1000, height=500
     st.subheader(":smile: 귀하의 합격을 기원합니다! :smile:")
     
   with tab1 :
