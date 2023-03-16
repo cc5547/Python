1b6ce7eedf4cce8c09f72bfc9611ff59014b8361
cc5547 <126434132+cc5547@users.noreply.github.com>
2023-03-16 17:20:50 +0900

    go

--- submission/cc5547/02_second_webapp/app.py
+++ submission/cc5547/02_second_webapp/app.py
@@ -56,6 +56,7 @@ def main():
     st.title(':smile: 시험장소를 안내해드립니다 :smile:')
     # 데이터프레임 출력 및 사이즈 조절
     st.dataframe(result, width=1000, height=500) # table로도 작성해볼것
+    st.subheader(":smile: 귀하의 시험을 응원합니다! :smile:")
     
   with tab1 :
     # tab1 에 담을 내용
