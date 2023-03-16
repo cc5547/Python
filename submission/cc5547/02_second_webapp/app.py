537469ecc9e7983177a45e98ae42880318df1c5a
cc5547 <126434132+cc5547@users.noreply.github.com>
2023-03-16 16:57:26 +0900

    go

--- submission/cc5547/02_second_webapp/app.py
+++ submission/cc5547/02_second_webapp/app.py
@@ -51,7 +51,7 @@ def main():
   with col :
     # column 에 담을 내용
     st.title(':smile: 시험장소를 안내해드립니다 :smile:')
-    if result in not None:
+    if result is not None:
       # 데이터프레임 출력 및 사이즈 조절
       st.dataframe(result, width=1000, height=500) # table로도 작성해볼것
     else : 
