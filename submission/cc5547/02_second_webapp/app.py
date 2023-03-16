4246ba07c34f95d69031695140efdf6addf57b0c
cc5547 <126434132+cc5547@users.noreply.github.com>
2023-03-16 11:55:59 +0900

    go

--- submission/cc5547/02_second_webapp/app.py
+++ submission/cc5547/02_second_webapp/app.py
@@ -46,15 +46,7 @@ def main():
     st.write(df.head(40))
   
   with tab1 :
-    chart_data = pd.DataFrame(
-    np.random.randn(20, 3),
-    columns=['a', 'b', 'c'])
-
-    c = alt.Chart(chart_data).mark_circle().encode(
-    x='a', y='b', size='c', color='c', tooltip=['ㄱ', 'b', 'c'])
-
-    st.altair_chart(c, use_container_width=True)
-
+    pass
   with tab2 :
     pass
 
