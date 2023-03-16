47ffda662b76fa6f02893bce3c314d6f6eb7f291
cc5547 <126434132+cc5547@users.noreply.github.com>
2023-03-16 14:45:26 +0900

    go

--- submission/cc5547/02_second_webapp/app.py
+++ submission/cc5547/02_second_webapp/app.py
@@ -10,7 +10,7 @@ def create_set():
   # 탭 생성 : 첫번째 탭의 이름은 Tab A 로, Tab B로 표시합니다. 
   tab1, tab2= st.tabs(['Tab_1' , 'Tab_2'])
 
-def create_df:
+def create_df():
   # DF_URL
   df_URL = "https://raw.githubusercontent.com/cc5547/Python/main/submission/cc5547/02_second_webapp/%EC%8B%9C%ED%97%98%EC%9E%A5%EC%86%8C_%EA%B0%80%EA%B3%B5%EC%B2%98%EB%A6%AC.csv"
   # df언네임 삭제
