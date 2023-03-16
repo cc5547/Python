cb402c02fc80d72ab65cddb5ab8ff02576f4d044
cc5547 <126434132+cc5547@users.noreply.github.com>
2023-03-16 11:52:18 +0900

    해

--- submission/cc5547/02_second_webapp/app.py
+++ submission/cc5547/02_second_webapp/app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import altair as alt
 
 # 중앙 정렬
 st.set_page_config(layout="wide") 
@@ -45,7 +46,14 @@ def main():
     st.write(df.head(40))
   
   with tab1 :
-    pass
+    chart_data = pd.DataFrame(
+    np.random.randn(20, 3),
+    columns=['a', 'b', 'c'])
+
+    c = alt.Chart(chart_data).mark_circle().encode(
+    x='a', y='b', size='c', color='c', tooltip=['a', 'b', 'c'])
+
+st.altair_chart(c, use_container_width=True)
 
   with tab2 :
     pass
