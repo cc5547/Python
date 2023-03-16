20061a0eeb3c8825da88a26c22846f5024266d5b
cc5547 <126434132+cc5547@users.noreply.github.com>
2023-03-16 10:52:18 +0900

    go

--- submission/cc5547/02_second_webapp/app.py
+++ submission/cc5547/02_second_webapp/app.py
@@ -23,7 +23,7 @@ def side_bar(s) :
   s.title('여기가 사이드바 입니다. \n 지역(특별시, 광역시, 시 ...)')
   language = ['서울특별시', '인천광역시', '경기도']
 
-  choice = s.selectbox('지역 선택', language, default='')
+  choice = s.selectbox('지역 선택', language, default='---')
 
   if choice == language[0] :
     st.write('서울을 선택하셨습니다.')
