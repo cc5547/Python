c338c61940d667aeeb891b236b3b63c6534d41f0
cc5547 <126434132+cc5547@users.noreply.github.com>
2023-03-16 12:25:27 +0900

    go

--- submission/cc5547/02_second_webapp/app.py
+++ submission/cc5547/02_second_webapp/app.py
@@ -9,7 +9,9 @@ st.set_page_config(layout="wide")
 # DF_URL
 df_URL = "https://raw.githubusercontent.com/cc5547/Python/main/submission/cc5547/02_second_webapp/%ED%95%9C%EA%B5%AD%EC%82%B0%EC%97%85%EC%9D%B8%EB%A0%A5%EA%B3%B5%EB%8B%A8_%EC%8B%9C%ED%97%98%EC%9E%A5%EC%86%8C.csv"
 # df언네임 삭제 
-df = pd.read_csv(df_URL).iloc[:, 1:].index += 1
+df = pd.read_csv(df_URL).iloc[:, 1:]
+# df인덱스 올림
+df.index += 1
 # 단일 컬럼 생성
 col = st.columns(1)[0]
 
