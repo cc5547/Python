224776dbf9cd7aa80ca6505a124ef0f4120ffd9d
cc5547 <126434132+cc5547@users.noreply.github.com>
2023-03-17 12:34:00 +0900

    go

--- submission/cc5547/02_second_webapp/app.py
+++ submission/cc5547/02_second_webapp/app.py
@@ -4,28 +4,16 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-# import plotly.graph_objects as go
-# import plotly.express as px
-# import matplotlib.pyplot as plt 
-
-# 중앙 정렬
-st.set_page_config(layout="wide") 
-# 8:2 비율의 컬럼 생성
-col1, col2 = st.columns([8, 2])
-# 탭 생성 : 첫번째 탭의 이름은 Tab_1로, Tab_2로 표시
-tab1, tab2= st.tabs(['필기 년도 별 합격률' , '응시자 및 합격자 수'])
-
-
 # 데이터 프레임 생성
 def create_df():
   df_URL = "https://raw.githubusercontent.com/cc5547/Python/main/submission/cc5547/02_second_webapp/%EC%8B%9C%ED%97%98%EC%9E%A5%EC%86%8C_%EA%B0%80%EA%B3%B5%EC%B2%98%EB%A6%AC.csv"
-  df_URL_g_1 = "https://raw.githubusercontent.com/cc5547/Python/main/submission/cc5547/02_second_webapp/%EA%B7%B8%EB%9E%98%ED%94%84_1.csv"
+  # df_URL_g_1 = "https://raw.githubusercontent.com/cc5547/Python/main/submission/cc5547/02_second_webapp/%EA%B7%B8%EB%9E%98%ED%94%84_1.csv"
   
   df = pd.read_csv(df_URL).iloc[:, 1:]
-  df_g_1 = pd.read_csv(df_URL_g_1)
+  # df_g_1 = pd.read_csv(df_URL_g_1)
   df.index += 1
-
-  return df, df_g_1
+  
+  return df
 
 # 사이드바
 def side_bar(df) :
@@ -51,11 +39,17 @@ def create_graph(image_url):
   return image
 
 def main():
-  df, df_g_1 = create_df() 
-  df, result = side_bar(df) 
+  # 중앙 정렬
+  st.set_page_config(layout="wide") 
+  # 8:2 비율의 컬럼 생성
+  col1, col2 = st.columns([8, 2])
+  # 탭 생성 : 첫번째 탭의 이름은 Tab_1로, Tab_2로 표시
+  tab1, tab2= st.tabs(['필기 년도 별 합격률' , '응시자 및 합격자 수'])
+
+  df, result = side_bar(create_df()) 
 
   with col1 :
-    st.title(':smile: 시험장소를 안내해드립니다 :smile:') 
+    st.title(':smile: 시험장소를 안내해드립니다 :smile:')
     st.dataframe(result, width=800, height=500)
     st.subheader(":smile: 귀하의 합격을 기원합니다! :smile:")
 
