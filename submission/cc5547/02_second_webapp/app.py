f9719cd8dd69a37895edb8bab625858238bcbacf
cc5547 <126434132+cc5547@users.noreply.github.com>
2023-03-17 12:34:43 +0900

    go

--- submission/cc5547/02_second_webapp/app.py
+++ submission/cc5547/02_second_webapp/app.py
@@ -4,6 +4,13 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+# 중앙 정렬
+  st.set_page_config(layout="wide") 
+  # 8:2 비율의 컬럼 생성
+  col1, col2 = st.columns([8, 2])
+  # 탭 생성 : 첫번째 탭의 이름은 Tab_1로, Tab_2로 표시
+  tab1, tab2= st.tabs(['필기 년도 별 합격률' , '응시자 및 합격자 수'])
+
 # 데이터 프레임 생성
 def create_df():
   df_URL = "https://raw.githubusercontent.com/cc5547/Python/main/submission/cc5547/02_second_webapp/%EC%8B%9C%ED%97%98%EC%9E%A5%EC%86%8C_%EA%B0%80%EA%B3%B5%EC%B2%98%EB%A6%AC.csv"
@@ -39,13 +46,6 @@ def create_graph(image_url):
   return image
 
 def main():
-  # 중앙 정렬
-  st.set_page_config(layout="wide") 
-  # 8:2 비율의 컬럼 생성
-  col1, col2 = st.columns([8, 2])
-  # 탭 생성 : 첫번째 탭의 이름은 Tab_1로, Tab_2로 표시
-  tab1, tab2= st.tabs(['필기 년도 별 합격률' , '응시자 및 합격자 수'])
-
   df, result = side_bar(create_df()) 
 
   with col1 :
