ddefe7648b42cdfb751750a1f572c72022f9d344
cc5547 <126434132+cc5547@users.noreply.github.com>
2023-03-19 02:13:23 +0900

    go

--- submission/cc5547/02_second_webapp/app.py
+++ submission/cc5547/02_second_webapp/app.py
@@ -29,7 +29,8 @@ def side_bar(df) :
   search = s_bar.text_input('상세 검색 (시, 교명등의 키워드를 입력 :smile:)', value = "")
   result = df[(df['지사명'] == choice) & (df['시험장소'].str.contains(search))]
   result.index = np.arange(1, len(result) + 1) 
-  search.empty()
+  if st.button('Clear'):
+    search_input.empty()
   return result
 
 # 그래프 생성
