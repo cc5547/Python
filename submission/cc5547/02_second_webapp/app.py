bafda4123a77ced819fc481f807697512a6fd55c
cc5547 <126434132+cc5547@users.noreply.github.com>
2023-03-19 01:29:44 +0900

    go

--- submission/cc5547/02_second_webapp/app.py
+++ submission/cc5547/02_second_webapp/app.py
@@ -43,21 +43,24 @@ def main():
   df = create_df()
   result = side_bar(df)
   
-  col1, col2 = st.columns([8, 2])   
   
+
+
+  col1, col2 = st.columns([8, 2])   
   with col1 :
     st.title(":smile: 시험장소를 안내해드립니다 :smile:")
     st.dataframe(result, width=800, height=500)
     st.subheader(":smile: 귀하의 합격을 기원합니다! :smile:")
-
   with col2 : 
     st.markdown("[![Foo](https://i.imgur.com/SywJPmA.png)](https://map.naver.com/)")
+
+
+
 
   tab1, tab2= st.tabs(['필기 년도 별 합격률' , '응시자 및 합격자 수'])
   with tab1 : 
     image_url = "https://i.imgur.com/wOY7lUx.png"
     st.image(create_graph(image_url), use_column_width=True)
-    
   with tab2 : 
     image_url = "https://i.imgur.com/C9nrLkC.png"
     st.image(create_graph(image_url), use_column_width=True)
