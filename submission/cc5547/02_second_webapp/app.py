4e09680706201dfeb28cefc749ebc2e0d89cf35d
cc5547 <126434132+cc5547@users.noreply.github.com>
2023-03-19 00:51:46 +0900

    go

--- submission/cc5547/02_second_webapp/app.py
+++ submission/cc5547/02_second_webapp/app.py
@@ -24,7 +24,12 @@ def side_bar(df) :
       result = df[df['지사명'] == area[i]]
     else : pass
 
+  search = s_bar.text_input('상세 검색 (시, 교명등의 키워드를 입력 :smile:)', value=search)
+  text_input = st.empty()
+  text_input.on_submit(clear_input)
+
   search = s_bar.text_input('상세 검색 (시, 교명등의 키워드를 입력 :smile:)')
+
   
   # on_submit 이벤트 핸들러 등록
   def clear_input(sender):
