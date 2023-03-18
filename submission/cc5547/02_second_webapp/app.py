a3e306f0b3074732d0ab8b69b7478dc13749a5b6
cc5547 <126434132+cc5547@users.noreply.github.com>
2023-03-18 23:41:32 +0900

    go

--- submission/cc5547/02_second_webapp/app.py
+++ submission/cc5547/02_second_webapp/app.py
@@ -19,12 +19,13 @@ def side_bar(df) :
   s_bar.title('지역을 선택해주세요.')
   area = df['지사명'].drop_duplicates().tolist()
   choice = s_bar.selectbox('지역 선택(재검색시 상세 검색을 지워 주세요)', area, index = 10)
-  last_choice = choice
+  
   for i in range(len(area)):
     if choice == area[i]: 
       result = df[df['지사명'] == area[i]]
     else : pass
 
+  last_choice = choice
   if choice != last_choice : result = ""
   else : pass
 
