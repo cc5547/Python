7a07e0e9bd04b94c082fd23e70acde0d3da7f91a
cc5547 <126434132+cc5547@users.noreply.github.com>
2023-03-18 23:44:58 +0900

    go

--- submission/cc5547/02_second_webapp/app.py
+++ submission/cc5547/02_second_webapp/app.py
@@ -23,14 +23,12 @@ def side_bar(df) :
   for i in range(len(area)):
     if choice == area[i]: 
       result = df[df['지사명'] == area[i]]
+      search = ""
     else : pass
-
-  last_choice = choice
-  if choice != last_choice : result = ""
-  else : pass
 
   search = s_bar.text_input('상세 검색 (시, 교명등의 키워드를 입력 :smile:)', value="")
   result = df[(df['지사명'] == choice) & (df['시험장소'].str.contains(search))]
+  
   result.index = np.arange(1, len(result) + 1) 
 
   return result
