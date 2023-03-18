7ec86b8b7695a572927bbd460a5fe0cacabbd6ef
cc5547 <126434132+cc5547@users.noreply.github.com>
2023-03-19 01:36:29 +0900

    go

--- submission/cc5547/02_second_webapp/app.py
+++ submission/cc5547/02_second_webapp/app.py
@@ -45,7 +45,7 @@ def main():
   col1, col2 = st.columns([8, 2])   
   with col1 :
     st.title(":smile: 시험장소를 안내해드립니다 :smile:")
-    st.dataframe(result, width=800, height=500)
+    st.dataframe(result, width=1000, height=500)
     st.subheader(":smile: 귀하의 합격을 기원합니다! :smile:")
   with col2 : 
     st.markdown("[![Foo](https://i.imgur.com/SywJPmA.png)](https://map.naver.com/)")
