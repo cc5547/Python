57132fa915c99f3e0d9cce339ae1c810c9934d78
cc5547 <126434132+cc5547@users.noreply.github.com>
2023-03-19 00:17:28 +0900

    go

--- submission/cc5547/02_second_webapp/app.py
+++ submission/cc5547/02_second_webapp/app.py
@@ -27,7 +27,7 @@ def side_bar(df) :
   search = s_bar.text_input('상세 검색 (시, 교명등의 키워드를 입력 :smile:)', key="search")
   if search :
     result = df[(df['지사명'] == choice) & (df['시험장소'].str.contains(search))]
-    result.index = np.arange(1, len(result) + 1) 
+  result.index = np.arange(1, len(result) + 1) 
   if st.button("초기화"):
     s_bar.empty()
     search = ""
