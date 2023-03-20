7a3fe8fcd67258fc2847b6e8eb2327a974cac77e
cc5547 <126434132+cc5547@users.noreply.github.com>
2023-03-20 15:18:17 +0900

    go

--- submission/cc5547/02_second_webapp/app.py
+++ submission/cc5547/02_second_webapp/app.py
@@ -101,6 +101,17 @@ def main():
     st.subheader(":smile: 귀하의 합격을 기원합니다! :smile:")
   with col2 : st.markdown("[![Foo](https://i.imgur.com/SywJPmA.png)](https://map.naver.com/)")
 
+    # 사용자로부터 입력받은 텍스트를 저장할 변수
+    text = st.text_input("Enter your text here")
+
+    # 엔터 키가 눌렸는지 확인하고, 눌렸으면 입력창을 공백으로 만듦
+    if st.button("Submit") or (text and st.session_state.last_key_pressed == "Enter"):
+      st.write("입력한 텍스트: ", text)
+      st.session_state.last_key_pressed = ""
+    else : st.session_state.last_key_pressed = st.session_state.last_key_pressed or ""
+
+
+
   tab1, tab2 = st.tabs(['필기 년도 별 합격률' , '응시자 및 합격자 수'])
   with tab1 : 
     st.plotly_chart(create_graph(df_g1, None))
