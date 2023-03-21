0603ac39c97217ec4340baa54190ebeed4e326f5
cc5547 <126434132+cc5547@users.noreply.github.com>
2023-03-21 14:07:41 +0900

    go

--- submission/cc5547/03_third_webapp/app.py
+++ submission/cc5547/03_third_webapp/app.py
@@ -11,11 +11,11 @@ def handler():
         name = st.text_input('이름')
         score = st.number_input('점수')
         if st.button('추가') :
-            name_list[0] = name
-            score_list[0] = score
+            name_list.append(name)
+            score_list.append(score)
             st.write('이름: {}, 점수: {}'.format(name, score))
     with col2 : 
         if st.button('종료'):
-            st.write(f'이름: {name_list[0]}, 점수: {score_list[0]}')
+            st.write(f'이름: {name_list}, 점수: {score_list}')
 
 handler()
