569de29acfebe1bf8952c44f89f38cd8a6d7237d
cc5547 <126434132+cc5547@users.noreply.github.com>
2023-03-21 14:18:58 +0900

    g

--- submission/cc5547/03_third_webapp/app.py
+++ submission/cc5547/03_third_webapp/app.py
@@ -1,23 +1,10 @@
-import streamlit as st
-
-
+def create_df():
+    return pass
+def create_graph():
+    return pass
 def handler():
-    name_list = []
-    score_list = []
+    create_df()
+    create_graph()
+    return pass
 
-    col1, col2 = st.columns([8, 2])   
-    with col1 :
-        st.write('이름과 점수를 입력하세요.')
-        name = st.text_input('이름')
-        score = st.number_input('점수')
-
-        if st.button('추가') :
-            name_list.append(name)
-            score_list.append(score)
-            st.write('이름: {}, 점수: {}'.format(name, score))
-        else : pass
-    with col2 : 
-        if st.button('종료'):
-            st.write(f'이름: {name_list}, 점수: {score_list}')
-        else : pass
 handler()
