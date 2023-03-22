c58b802a9d831a45bda2c952927ff1e7e9760f68
cc5547 <126434132+cc5547@users.noreply.github.com>
2023-03-22 19:46:36 +0900

    해

--- submission/cc5547/02_second_webapp/app.py
+++ submission/cc5547/02_second_webapp/app.py
@@ -36,7 +36,7 @@ def side_bar(df) :
 
   search = s_bar.text_input('상세 검색 (시, 교명등의 키워드를 입력 :smile:)', value = '')
   result = df[(df['지사명'] == choice) & (df['시험장소'].str.contains(search))]
-  result.index = np.arange(1, len(result) + 1) 
+  result.index = np.arange(1, len(result) + 1)
 
   return result
 
@@ -89,16 +89,19 @@ def create_graph(df_g1, df_g2):
 def main():
   df, df_g1, df_g2 = create_df()
   
-  col1, col2 = st.columns([8, 2])   
+  col1, col2 = st.columns([8, 2])
   with col1 :
     st.title(":smile: 시험장소를 안내해드립니다 :smile:")
     st.dataframe(side_bar(df), width=1000, height=500)
     st.subheader(":smile: 귀하의 합격을 기원합니다! :smile:")
-  with col2 : st.markdown("[![Foo](https://i.imgur.com/SywJPmA.png)](https://map.naver.com/)")
+  with col2 : 
+    st.markdown("[![Foo](https://i.imgur.com/SywJPmA.png)](https://map.naver.com/)")
 
   tab1, tab2 = st.tabs(['필기 년도 별 합격률' , '응시자 및 합격자 수'])
-  with tab1 : st.plotly_chart(create_graph(df_g1, None))
-  with tab2 : st.plotly_chart(create_graph(None, df_g2))
+  with tab1 : 
+    st.plotly_chart(create_graph(df_g1, None))
+  with tab2 : 
+    st.plotly_chart(create_graph(None, df_g2))
 
 if __name__ == '__main__':
   main()
