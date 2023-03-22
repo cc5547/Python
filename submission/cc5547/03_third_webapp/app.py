7f417e6c57a87e4fcc49927694670ae29ae21bea
cc5547 <126434132+cc5547@users.noreply.github.com>
2023-03-22 20:24:10 +0900

    go

--- submission/cc5547/03_third_webapp/app.py
+++ submission/cc5547/03_third_webapp/app.py
@@ -1,17 +1,17 @@
 import streamlit as st
-from PIL import Image
-import requests
-import pandas as pd
-import numpy as np
-import plotly.express as px
-import plotly.graph_objects as go
+# from PIL import Image
+# import requests
+# import pandas as pd
+# import numpy as np
+# import plotly.express as px
+# import plotly.graph_objects as go
 
 
-def create_df() :
-    pass
+# def create_df() :
+#     pass
 
-def create_graph() :
-    pass
+# def create_graph() :
+#     pass
 
 def main() :
     col1, col2, col3, col4, col5 = ([2, 2, 2, 2, 2])
