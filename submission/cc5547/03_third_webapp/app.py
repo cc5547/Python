d5d58a302cc44a3462fa94766491923885c79531
cc5547 <126434132+cc5547@users.noreply.github.com>
2023-03-22 20:26:46 +0900

    해

--- submission/cc5547/03_third_webapp/app.py
+++ submission/cc5547/03_third_webapp/app.py
@@ -15,6 +15,7 @@ import streamlit as st
 
 def main() :
     col1, col2, col3, col4, col5 = ([2, 2, 2, 2, 2])
+
     with col1 :
         ## Title
         st.title("treamlit Tutorial")
@@ -23,6 +24,7 @@ def main() :
         st.subheader("This is subheader")
         ## Text
         st.text("Hello Streamlit! 이 글은 튜토리얼 입니다.")
+
     with col2 :
         ## Markdown syntax
         st.markdown("# This is a Markdown title")
@@ -38,10 +40,11 @@ def main() :
                     "   2. item 1.2\n"
                     "2. item 2\n"
                     "3. item 3")
-    with col3 : 
-       ## Checkbox
+    with col3 :
+        ## Checkbox
         if st.checkbox("Show/Hide"):
             st.write("체크박스가 선택되었습니다.")
+
     with col4 :
         ## Radio button
         status = st.radio("Select status.", ("Active", "Inactive"))
