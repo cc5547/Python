4f09b7f7e9e0b216be9b053d5ca05f193dfa203d
cc5547 <126434132+cc5547@users.noreply.github.com>
2023-03-26 22:52:35 +0900

    go

--- submission/cc5547/03_third_webapp/DataFrame.py
+++ submission/cc5547/03_third_webapp/DataFrame.py
@@ -7,8 +7,11 @@ class CreateDataFrame:
         self.df_URL = "https://raw.githubusercontent.com/cc5547/Python/main/submission/cc5547/02_second_webapp/%EC%8B%9C%ED%97%98%EC%9E%A5%EC%86%8C_%EA%B0%80%EA%B3%B5%EC%B2%98%EB%A6%AC.csv"
     
     # read
-    def load_df(self) :
-        df = pd.read_csv(self.df_URL)
+    def load_df(self):
+        try:
+            df = pd.read_csv(self.df_URL)
+        except pd.errors.ParserError:
+            df = pd.DataFrame("로드 실패")
         return df
 
     # df 전처리 해줄 거 작성 하기
