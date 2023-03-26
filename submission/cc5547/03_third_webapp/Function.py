f8eab876cd80dd174e45decbfd443f758fc24bea
cc5547 <126434132+cc5547@users.noreply.github.com>
2023-03-26 18:10:29 +0900

    go

--- submission/cc5547/03_third_webapp/Function.py
+++ submission/cc5547/03_third_webapp/Function.py
@@ -12,11 +12,11 @@ class SideBar:
         s.title('여기가 타이틀')
 
         area = self.df['지사명'].unique().tolist()
-        choice = s.selectbox('선택', area, index = 10)
+        choice = s.selectbox('선택', area, index=10)
 
-        result = self.df[df['지사명'] == choice]
+        result = self.df[self.df['지사명'] == choice]
         result.index = np.arange(1, len(result) + 1)
-        
+
         return result
 
     def ment(self):
