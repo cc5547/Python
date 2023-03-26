2e047dd1b41c626b774d1147065526feb03ae1ff
cc5547 <126434132+cc5547@users.noreply.github.com>
2023-03-26 14:54:32 +0900

    go

--- submission/cc5547/03_third_webapp/User_Interface.py
+++ submission/cc5547/03_third_webapp/User_Interface.py
@@ -6,6 +6,8 @@ import plotly.graph_objects as go
 # from PIL import Image
 # import requests
 
+st.set_page_config(layout="wide")
+
 # df생성
 def create_df() :
     df_URL = "https://raw.githubusercontent.com/cc5547/Python/main/submission/cc5547/02_second_webapp/%EC%8B%9C%ED%97%98%EC%9E%A5%EC%86%8C_%EA%B0%80%EA%B3%B5%EC%B2%98%EB%A6%AC.csv"
