fc363f8917e732325500fe101bbf5e1be2d9f23e
cc5547 <126434132+cc5547@users.noreply.github.com>
2023-03-26 23:31:40 +0900

    go

--- submission/cc5547/03_third_webapp/SideBar.py
+++ submission/cc5547/03_third_webapp/SideBar.py
@@ -13,16 +13,19 @@ class Sidebar:
 
         area = self.df['지사명'].drop_duplicates().tolist()
         choice = self.s.selectbox('선택', area)
+
         return choice
 
     # 셀렉트 박스 선택 결과 처리 
     def select_choice(self, choice):
         result = self.df[self.df['지사명'] == choice]
         result.index = np.arange(1, len(result) + 1)
+
         return result
 
     # 후에 유지보수시 한번에 return 하는 용으로 만듬
     def result_sidebar(self):
         choice = self.create_sidebar()
         result = self.select_choice(choice)
+        
         return result
