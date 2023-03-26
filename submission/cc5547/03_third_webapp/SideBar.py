bf073f3ee39d1bc1e70fcb3b9d7d9b8a87dc64ba
cc5547 <126434132+cc5547@users.noreply.github.com>
2023-03-26 22:39:08 +0900

    go

--- submission/cc5547/03_third_webapp/SideBar.py
+++ submission/cc5547/03_third_webapp/SideBar.py
@@ -12,7 +12,7 @@ class Sidebar:
         self.s.title('여기가 타이틀')
 
         area = self.df['지사명'].drop_duplicates().tolist()
-        choice = self.s.selectbox('선택', area, index = area.index('서울특별시'))
+        choice = self.s.selectbox('선택', area, index = area[10])
         return choice
 
     # 셀렉트 박스 선택 결과 처리 
