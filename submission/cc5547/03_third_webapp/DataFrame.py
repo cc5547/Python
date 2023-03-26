222d3f99e1c633ddd7b1ba095a011f3e308c9f23
cc5547 <126434132+cc5547@users.noreply.github.com>
2023-03-26 19:00:43 +0900

    go

--- submission/cc5547/03_third_webapp/DataFrame.py
+++ submission/cc5547/03_third_webapp/DataFrame.py
@@ -2,13 +2,16 @@ import pandas as pd
 
 # df 생성
 class CreateDataFrame:
+    # df URL
     def __init__(self) -> None:
         self.df_URL = "https://raw.githubusercontent.com/cc5547/Python/main/submission/cc5547/02_second_webapp/%EC%8B%9C%ED%97%98%EC%9E%A5%EC%86%8C_%EA%B0%80%EA%B3%B5%EC%B2%98%EB%A6%AC.csv"
     
+    # read
     def load_df(self) :
         df = pd.read_csv(self.df_URL).iloc[:, 1:]
         return df
 
+    # df 전처리 해줄 거 작성 하기
     def create_df(self) :
         df = self.load_df()
         df.index += 1
