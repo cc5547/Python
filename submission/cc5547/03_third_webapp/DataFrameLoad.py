0873d1a83daf0162cbef414aacfb2734308b9a7c
cc5547 <126434132+cc5547@users.noreply.github.com>
2023-03-26 17:02:24 +0900

    go

--- submission/cc5547/03_third_webapp/DataFrameLoad.py
+++ submission/cc5547/03_third_webapp/DataFrameLoad.py
@@ -5,13 +5,13 @@ class DataFrameLoader:
     def __init__(self) -> None:
         self.df_URL = "https://raw.githubusercontent.com/cc5547/Python/main/submission/cc5547/02_second_webapp/%EC%8B%9C%ED%97%98%EC%9E%A5%EC%86%8C_%EA%B0%80%EA%B3%B5%EC%B2%98%EB%A6%AC.csv"
     
+    def df_index(self) :
+        df.index += 1
+        return df
+        
     def create_df(self) :
         df = pd.read_csv(self.df_URL).iloc[:, 1:]
         self.df_index(self)
-        return df
-
-    def df_index(self) :
-        df.index += 1
         return df
 
 # @st.cache
