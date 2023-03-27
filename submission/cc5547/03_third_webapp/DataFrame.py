a638f846b48960cdd24cc067df0f5d80097a0c45
cc5547 <126434132+cc5547@users.noreply.github.com>
2023-03-27 10:14:17 +0900

    go

--- submission/cc5547/03_third_webapp/DataFrame.py
+++ submission/cc5547/03_third_webapp/DataFrame.py
@@ -9,7 +9,7 @@ class CreateDataFrame:
     
     # read / df 전처리 해줄 거 작성 하기
     def load_df(self):
-        df = pd.read_csv(df_URL).iloc[:, 1:]
+        df = pd.read_csv(self.df_URL).iloc[:, 1:]
         df.index += 1
         return df
 
