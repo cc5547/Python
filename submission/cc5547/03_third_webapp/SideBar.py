2bb14104ed157e373ddc20ac2507eb7cacc9f0e7
cc5547 <126434132+cc5547@users.noreply.github.com>
2023-03-28 14:37:37 +0900

    go

--- submission/cc5547/03_third_webapp/SideBar.py
+++ submission/cc5547/03_third_webapp/SideBar.py
@@ -8,24 +8,6 @@ class Sidebar:
         self.space = ""
         self.gender_c = ['남자', '여자']
         self.y_n = ['예', '아니오']
-    
-    # 나이 받기 float
-    def account_age(self):
-        self.s.title('문진표를 작성해 주세요👇')
-        age = self.s.text_input('나이를 입력해 주세요.', self.space)
-        return age
-
-    # 성별 받기 float
-    def gender_choice(self):
-        gender = self.s.radio('성별을 선택 해주세요.', self.gender_c)
-        if gender == "남자" : return 1
-        else : return 0
-
-    # 심장병 유무
-    def heart_sick(self):
-        heart = self.s.radio('심장병이 있습니까?', self.y_n)
-        if heart == "예" : return True 
-        else : return False
 
     # 혈압 int
     def blood_pressure(self) :
@@ -42,14 +24,31 @@ class Sidebar:
         hbit = self.s.slider('심박수를 입력하세요', 0, 200, 1)
         return hbit
 
+    # 성별 받기 float
+    def gender_choice(self):
+        gender = self.s.radio('성별을 선택 해주세요.', self.gender_c)
+        if gender == "남자" : return 1
+        else : return 0
+
+    # 심장병 유무
+    def heart_sick(self):
+        heart = self.s.radio('심장병이 있습니까?', self.y_n)
+        if heart == "예" : return True 
+        else : return False
+
+    # 나이 받기 float
+    def account_age(self):
+        self.s.title('문진표를 작성해 주세요👇')
+        age = self.s.text_input('나이를 입력해 주세요.', self.space)
+        return age
 
     # 후에 유지보수시 한번에 return 하는 용으로 만듬 main에서 이 함수를 호출함.
     def result_sidebar(self):
-        age = self.account_age()
-        gender = self.gender_choice()
-        heart = self.heart_sick()
         blood = self.blood_pressure()
         clst = self.cholesterol()
         hbit = self.heart_beat()
+        gender = self.gender_choice()
+        heart = self.heart_sick()
+        age = self.account_age()
 
-        return age, gender, heart, blood, clst, hbit
+        return blood, clst, hbit, gender, heart, age
