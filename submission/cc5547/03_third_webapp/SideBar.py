b31349f3f356733c80d027a411814abc032197b3
cc5547 <126434132+cc5547@users.noreply.github.com>
2023-03-28 09:36:47 +0900

    go

--- submission/cc5547/03_third_webapp/SideBar.py
+++ submission/cc5547/03_third_webapp/SideBar.py
@@ -8,21 +8,25 @@ class Sidebar:
         self.s = st.sidebar
         self.on = True
         self.off = False
+        self.name = None
     # 셀렉트 박스 생성
     def account_name(self):
         self.s.title('문진표를 작성해 주세요👇')
         
-        name = self.s.text_input('이름을 입력해 주세요.')
-        return name
+        self.name = self.s.text_input('이름을 입력해 주세요.')
+        return self.name
 
     # 셀렉트 박스 선택 결과 처리 
     def first_choice(self, name):
         if name is None:
             return None, self.off
+
         else : return name, self.on
 
     # 후에 유지보수시 한번에 return 하는 용으로 만듬 main에서 이 함수를 호출함.
     def result_sidebar(self):
         name = self.account_name()
+
         result, on_off = self.first_choice(name)
+        
         return result, on_off
