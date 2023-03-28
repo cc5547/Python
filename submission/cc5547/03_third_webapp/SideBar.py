58068425d8cb4165d74fb8d0ee8df2dd33479c98
cc5547 <126434132+cc5547@users.noreply.github.com>
2023-03-28 11:58:08 +0900

    go

--- submission/cc5547/03_third_webapp/SideBar.py
+++ submission/cc5547/03_third_webapp/SideBar.py
@@ -14,19 +14,14 @@ class Sidebar:
     def account_age(self):
         self.s.title('문진표를 작성해 주세요👇')
         age = self.s.text_input('나이를 입력해 주세요.', self.age)
-        return age if age else None
-
+        return age
     # 성별 받기
-    def sex_choice(self, age):
-        if age is None : return None
-        
+    def sex_choice(self):
         sex = self.s.selectbox('성별을 선택해주세요.', self.sex_c)
         return 1 if sex == '남자' else 0
 
     # 후에 유지보수시 한번에 return 하는 용으로 만듬 main에서 이 함수를 호출함.
     def result_sidebar(self):
         age = self.account_age()
-        sex = self.sex_choice(age)
-        if age is None or sex is None:
-            return None, None
+        sex = self.sex_choice()
         return age, sex
