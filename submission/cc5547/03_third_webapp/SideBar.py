7e83f70afdbceb8ba4f815d0b8efb851202b5774
cc5547 <126434132+cc5547@users.noreply.github.com>
2023-03-28 12:26:46 +0900

    go

--- submission/cc5547/03_third_webapp/SideBar.py
+++ submission/cc5547/03_third_webapp/SideBar.py
@@ -20,7 +20,7 @@ class Sidebar:
         gender = self.s.selectbox('성별을 선택해주세요.', self.gender_c)
         return 1 if gender == '남자' else 0
     
-    def 
+
     # 후에 유지보수시 한번에 return 하는 용으로 만듬 main에서 이 함수를 호출함.
     def result_sidebar(self):
         age = self.account_age()
