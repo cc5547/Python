4d7e244e44624301737b89248436ce3026e88a13
cc5547 <126434132+cc5547@users.noreply.github.com>
2023-03-28 19:59:24 +0900

    go

--- submission/cc5547/03_third_webapp/FuncTion.py
+++ submission/cc5547/03_third_webapp/FuncTion.py
@@ -4,14 +4,20 @@ import pandas as pd
 
 class Function:
     def __init__(self, data, blood, clst, hbit, gender, heart, age) -> None:
-        self.data = data.astype(np.float32)
-        self.blood = blood.astype(np.float32)
-        self.clst = clst.astype(np.float32)
-        self.hbit = hbit.astype(np.float32)
-        self.gender = gender.astype(np.float32)
-        self.heart = heart.astype(np.float32)
-        self.age = age.astype(np.float32)
-    
+        # self.data = data.astype(np.float32)
+        # self.blood = blood.astype(np.float32)
+        # self.clst = clst.astype(np.float32)
+        # self.hbit = hbit.astype(np.float32)
+        # self.gender = gender.astype(np.float32)
+        # self.heart = heart.astype(np.float32)
+        # self.age = age.astype(np.float32)
+        self.data = data
+        self.blood = blood
+        self.clst = clst
+        self.hbit = hbit
+        self.gender = gender
+        self.heart = heart
+        self.age = age
     def into_xgb_model(self):
         # 나이, 성별, 심장병, 혈압, 콜레스테롤, 최대심박수
         tf = self.data.predict([[self.age, self.gender, self.heart, self.blood, self.clst, self.hbit]])
