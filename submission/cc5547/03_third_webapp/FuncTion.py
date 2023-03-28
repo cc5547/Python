8ee0cde48b8822582f259e905daac67550ee1a78
cc5547 <126434132+cc5547@users.noreply.github.com>
2023-03-28 19:45:03 +0900

    go

--- submission/cc5547/03_third_webapp/FuncTion.py
+++ submission/cc5547/03_third_webapp/FuncTion.py
@@ -3,8 +3,8 @@ import numpy as np
 import pandas as pd
 
 class Function:
-    def __init__(self, data, blood, clst, hbit, gender, heart, age) -> None:
-        self.data = data
+    def __init__(self, blood, clst, hbit, gender, heart, age) -> None:
+        self.data = self
         self.blood = blood
         self.clst = clst
         self.hbit = hbit
