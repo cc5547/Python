9190f8d6503e0e254f8a2d239ddce8811cc294e1
cc5547 <126434132+cc5547@users.noreply.github.com>
2023-03-28 20:31:25 +0900

    go

--- submission/cc5547/03_third_webapp/FuncTion.py
+++ submission/cc5547/03_third_webapp/FuncTion.py
@@ -5,26 +5,26 @@ import pandas as pd
 class Function:
     def __init__(self, data, blood, clst, hbit, gender, heart, age) -> None:
         self.data = data
-        self.blood = int(blood)
-        self.clst = int(clst)
-        self.hbit = int(hbit)
-        self.gender = float(gender)
-        self.heart = int(heart)
-        self.age = float(age)
+        self.blood = blood
+        self.clst = clst
+        self.hbit = hbit
+        self.gender = gender
+        self.heart = heart
+        self.age = age
+``
         # self.data = data
-        # self.blood = blood.astype(np.int64)
-        # self.clst = clst.astype(np.int64)
-        # self.hbit = hbit.astype(np.int64)
-        # self.gender = gender.astype(np.float32)
-        # self.heart = heart.astype(np.int64)
-        # self.age = age.astype(np.float32)
-
-        
+        # self.blood = int(blood)
+        # self.clst = int(clst)
+        # self.hbit = int(hbit)
+        # self.gender = float(gender)
+        # self.heart = int(heart)
+        # self.age = float(age)
+    
         
     def into_xgb_model(self):
         # 나이, 성별, 심장병, 혈압, 콜레스테롤, 최대심박수
-        tf = self.data.predict([[self.age, self.gender, self.heart, self.blood, self.clst, self.hbit]])
-        tf_p = self.data.predict_proba([[self.age, self.gender, self.heart, self.blood, self.clst, self.hbit]])
+        tf = self.data.predict([[float(self.age), float(self.gender), int(self.heart), int(self.blood), int(self.clst), int(self.hbit)]])
+        tf_p = self.data.predict_proba([[float(self.age), float(self.gender), int(self.heart), int(self.blood), int(self.clst), int(self.hbit)]])
         tf_p = (np.array(tf_p)[:,1])
         return tf, tf_p
 
