00fb57d3c861afafb41387847ae1276b481fb98d
cc5547 <126434132+cc5547@users.noreply.github.com>
2023-03-28 17:24:46 +0900

    go

--- submission/cc5547/03_third_webapp/Data.py
+++ submission/cc5547/03_third_webapp/Data.py
@@ -5,7 +5,7 @@ import pandas as pd
 class CreateData:
     # df URL
     def __init__(self) -> None:
-        # self.df_URL = "https://raw.githubusercontent.com/cc5547/Python/main/submission/cc5547/03_third_webapp/%EC%8B%9C%ED%97%98%EC%9E%A5%EC%86%8C_%EA%B0%80%EA%B3%B5%EC%B2%98%EB%A6%AC.csv"
+        self.df_URL = "https://raw.githubusercontent.com/cc5547/Python/main/submission/cc5547/02_second_webapp/%EC%8B%9C%ED%97%98%EC%9E%A5%EC%86%8C_%EA%B0%80%EA%B3%B5%EC%B2%98%EB%A6%AC.csv"
     
     # read / df 전처리 해줄 거 작성 하기
     def load_df(self):
