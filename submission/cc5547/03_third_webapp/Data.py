3eeaed114b76476f6487a644d35ac070ba72b455
cc5547 <126434132+cc5547@users.noreply.github.com>
2023-03-28 17:50:31 +0900

    raw

--- submission/cc5547/03_third_webapp/Data.py
+++ submission/cc5547/03_third_webapp/Data.py
@@ -6,7 +6,7 @@ import joblib
 class CreateData:
     # df URL
     def __init__(self) -> None:
-        self.jobs = "https://github.com/cc5547/Python/blob/main/submission/cc5547/03_third_webapp/xgb_model%20.joblib"
+        self.jobs = "https://github.com/cc5547/Python/raw/main/submission/cc5547/03_third_webapp/xgb_model%20.joblib"
 
     # read / df 전처리 해줄 거 작성 하기
     def load_df(self):
