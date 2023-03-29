623a456abb673f504ed83d3cab26a065436c9555
cc5547 <126434132+cc5547@users.noreply.github.com>
2023-03-29 11:28:12 +0900

    go

--- submission/cc5547/03_third_webapp/FuncTion.py
+++ submission/cc5547/03_third_webapp/FuncTion.py
@@ -16,7 +16,8 @@ class Function:
             # 나이, 성별, 심장병, 혈압, 콜레스테롤, 최대심박수
             tf = self.data.predict([[float(self.age), float(self.gender), int(self.heart), int(self.blood), int(self.clst), int(self.hbit)]])
             tf_p = self.data.predict_proba([[float(self.age), float(self.gender), int(self.heart), int(self.blood), int(self.clst), int(self.hbit)]])
-            # tf_p = (np.array(tf_p)[:,1])
+            
+            tf_p = (np.array(tf_p)[:,1])
             return tf, tf_p
 
     def create_model(self):
