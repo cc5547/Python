bc049d68ef4a3cb0c3b1d3222ef030000dc31658
cc5547 <126434132+cc5547@users.noreply.github.com>
2023-03-29 15:25:43 +0900

    go

--- submission/cc5547/03_third_webapp/Handle.py
+++ submission/cc5547/03_third_webapp/Handle.py
@@ -43,8 +43,8 @@ def user_interface(blood, clst, hbit, gender, heart, age) :
         st.write(f"### 👉 콜레스트롤 : {clst}TC")
         st.write(f"### 👉 심박수 : {hbit}bpm")
     with col2 : 
-        if tf == 1 : st.write("# 분석 결과 👉 <span style='color:red'>고혈압</span> 입니다.", unsafe_allow_html=True)
-        elif tf == 0 : st.write("# 분석 결과 👉 <span style='color:blue'>정상</span> 입니다.", unsafe_allow_html=True)
+        if tf == 0 : st.write("# 분석 결과 👉 <span style='color:red'>고혈압</span> 입니다.", unsafe_allow_html=True)
+        elif tf == 1 : st.write("# 분석 결과 👉 <span style='color:blue'>정상</span> 입니다.", unsafe_allow_html=True)
         st.write(f"# 결과에 실망하지 마세요😭 확률을 알려드립니다 👉 {', '.join([f'{p*100:.4f}%' for p in tf_p])}")
     
 # main 시작점
