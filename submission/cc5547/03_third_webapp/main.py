f83c275d482656d02e7eee788ad23d0b2d10392d
cc5547 <126434132+cc5547@users.noreply.github.com>
2023-03-29 23:52:12 +0900

    go

--- submission/cc5547/03_third_webapp/main.py
+++ submission/cc5547/03_third_webapp/main.py
@@ -9,7 +9,7 @@ def main():
 
     blood, clst, hbit, gender, heart, age = get.get_sidebar()
 
-    if age != "" : UI.user_interface(blood, clst, hbit, gender, heart, age)
+    if age != "" : UI.user_interface(get.get_sidebar())
     else : st.markdown(img_url, unsafe_allow_html=True)
     
 # 메인 실행
