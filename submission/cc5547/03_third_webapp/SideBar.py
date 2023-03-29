e9c125b11c310e0133fa4ba5fdc872ecbf351dc2
cc5547 <126434132+cc5547@users.noreply.github.com>
2023-03-29 23:13:46 +0900

    go

--- submission/cc5547/03_third_webapp/SideBar.py
+++ submission/cc5547/03_third_webapp/SideBar.py
@@ -26,15 +26,13 @@ class Sidebar:
     # 성별 받기 float로 후에 형변환
     def gender_choice(self, hbit):
         if hbit is not None and hbit > 1 : return 1 if self.s.radio('성별을 선택 해주세요.', self.gender_c) == "남자" else 0
-            # if self.s.radio('성별을 선택 해주세요.', self.gender_c) == "남자" : return 1
-            # else : return 0
         else : return None
         
     # 심장병 유무 int로 후에 형변환
     def heart_sick(self, hbit) : 
-        if hbit is not None and hbit > 1 :
-            if self.s.radio('심장병이 있습니까?', self.y_n) == "예" : return 0 
-            else : return 1
+        if hbit is not None and hbit > 1 : return 0 if self.s.radio('심장병이 있습니까?', self.y_n) == "예" else 1
+            # if self.s.radio('심장병이 있습니까?', self.y_n) == "예" : return 0 
+            # else : return 1
         else : return None
 
     # 나이 받기 float로 후에 형변환
