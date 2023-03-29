b823f15b3e9d123c8208aca21af36e144101caee
cc5547 <126434132+cc5547@users.noreply.github.com>
2023-03-29 15:46:31 +0900

    go

--- submission/cc5547/03_third_webapp/Handle.py
+++ submission/cc5547/03_third_webapp/Handle.py
@@ -55,7 +55,9 @@ def main():
     blood, clst, hbit, gender, heart, age = get_sidebar()
 
     if age != "" : user_interface(blood, clst, hbit, gender, heart, age)
-    else : st.markdown('<a href="https://www.notion.so/82e465017bfe45dd82bbf78b46f24469"><img src="https://i.imgur.com/ktulthH.gif" width=1000></a>', unsafe_allow_html=True)
+    else : st.markdown('<a href="https://sparkly-prince-933.notion.site/1ccb865a95e54590bfd61e22b45520fa"><img src="https://i.imgur.com/ktulthH.gif" width=1000></a>', unsafe_allow_html=True)
+    # 노션 옹되 = https://www.notion.so/82e465017bfe45dd82bbf78b46f24469
+    # 노션 쟂인 = https://sparkly-prince-933.notion.site/1ccb865a95e54590bfd61e22b45520fa
 if st.button(""):
             st.image("https://i.imgur.com/4Xd3Mdn.gif", width = 1400)
 # 메인 실행
