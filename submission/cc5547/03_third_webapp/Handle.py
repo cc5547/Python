9cc4488a776fc13fcdba6a082eb344ace9f9499a
cc5547 <126434132+cc5547@users.noreply.github.com>
2023-03-29 12:34:05 +0900

    go

--- submission/cc5547/03_third_webapp/Handle.py
+++ submission/cc5547/03_third_webapp/Handle.py
@@ -51,8 +51,7 @@ def user_interface(blood, clst, hbit, gender, heart, age):
     if tf == 1 : st.write("# 분석 결과 -> <span style='color:red'>고혈압</span> 입니다.", unsafe_allow_html=True)
     elif tf == 0 : st.write("# 분석 결과 -> <span style='color:blue'>정상</span> 입니다.", unsafe_allow_html=True)
     # st.write(f"# 당신 죽을 확률{tf_p}")
-    st.write(f"# 결과에 실망하지 마세요😭 확률을 알려드립니다 = {[f'{p*100:.4f}%' for p in tf_p]}")
-
+    st.write(f"# 결과에 실망하지 마세요😭 확률을 알려드립니다 = {', '.join([f'{p*100:.4f}%' for p in tf_p])}")
 
 # main 시작점
 def main():
