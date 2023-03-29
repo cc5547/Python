6e60fd4d9714ea3dad8c9d6fdc895ae377166776
cc5547 <126434132+cc5547@users.noreply.github.com>
2023-03-29 11:03:46 +0900

    go

--- submission/cc5547/03_third_webapp/Handle.py
+++ submission/cc5547/03_third_webapp/Handle.py
@@ -39,7 +39,7 @@ def user_interface(blood, clst, hbit, gender, heart, age):
         st.write(f"### 콜레스트롤 : {clst}세")
         st.write(f"### 심박수 : {hbit}세")
         if tf == 1 : 
-            st.write("# 분석 결과 <span style='color:red'>고혈압</span> 입니다.")
+            st.write("# 고혈압 분석 결과 <span style='color:red'>고혈압</span> 입니다.", unsafe_allow_html=True)
             # st.write("# 고혈압 분석 결과 고혈압 입니다.")
         elif tf == 0 : 
             st.write("# 분석 결과 <span style='color:red'>정상</span> 입니다.")
