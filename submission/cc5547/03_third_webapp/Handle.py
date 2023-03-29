75c2f9b475660a448b196d9fa8576ccca4aa1041
cc5547 <126434132+cc5547@users.noreply.github.com>
2023-03-29 10:29:42 +0900

    go

--- submission/cc5547/03_third_webapp/Handle.py
+++ submission/cc5547/03_third_webapp/Handle.py
@@ -29,7 +29,7 @@ def user_interface(blood, clst, hbit, gender, heart, age):
     data = get_data()
     tf, tf_p = get_function(data, blood, clst, hbit, gender, heart, age)
 
-    st.title("입력한 정보로 분석한 결과 입니다.🦾")
+    st.title("🦾 입력한 정보로 분석한 결과 입니다.")
     col1, col2 = st.columns([8, 2])
         
     with col1:
