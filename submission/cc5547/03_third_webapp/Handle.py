954554ed0820e026cf428e72d5f8feb28d66f0a7
cc5547 <126434132+cc5547@users.noreply.github.com>
2023-03-29 15:18:58 +0900

    go

--- submission/cc5547/03_third_webapp/Handle.py
+++ submission/cc5547/03_third_webapp/Handle.py
@@ -46,7 +46,6 @@ def user_interface(blood, clst, hbit, gender, heart, age) :
         if tf == 1 : st.write("# 분석 결과 👉 <span style='color:red'>고혈압</span> 입니다.", unsafe_allow_html=True)
         elif tf == 0 : st.write("# 분석 결과 👉 <span style='color:blue'>정상</span> 입니다.", unsafe_allow_html=True)
         st.write(f"# 결과에 실망하지 마세요😭 확률을 알려드립니다 👉 {', '.join([f'{p*100:.4f}%' for p in tf_p])}")
-        st.markdown('<a href="https://www.notion.so/82e465017bfe45dd82bbf78b46f24469"><img src="https://i.imgur.com/ktulthH.gif" width=1000></a>', unsafe_allow_html=True)
     
 # main 시작점
 def main():
@@ -55,6 +54,8 @@ def main():
 
     if age != "" : user_interface(blood, clst, hbit, gender, heart, age)
     else : st.markdown('<a href="https://www.notion.so/82e465017bfe45dd82bbf78b46f24469"><img src="https://i.imgur.com/ktulthH.gif" width=1000></a>', unsafe_allow_html=True)
+if st.button("마무리"):
+            st.image("https://i.imgur.com/4Xd3Mdn.gif", width = 1000)
 # 메인 실행
 if __name__ == '__main__':
     main()
