e868e6f3c8360eb36ab0f21a21bdede97c572ff5
cc5547 <126434132+cc5547@users.noreply.github.com>
2023-03-29 23:33:11 +0900

    go

--- submission/cc5547/03_third_webapp/Handle.py
+++ submission/cc5547/03_third_webapp/Handle.py
@@ -1,24 +1,19 @@
 from Data import CreateData
-from FuncTion import Function
 from SideBar import Sidebar
+from FuncTion import Function
 
 # Data, FuncTion, SideBar Class 들의 return 값들을 다 받아 오는 클래스 
 class Get:
     def __init__(self) -> None:
         self.dt = CreateData()
-        
+        self.sb = Sidebar()
+        self.fc = Function(data, int(blood), int(clst), int(hbit), float(gender), int(heart), float(age))
+
     # joblib불러오기
-    def get_data(self):
-        # dt = CreateData()
-        return self.dt.create_data()
+    def get_data(self) : return self.dt.create_data()
 
     # SideBar.py의 Sidebar 클래스의 result_sidebar()를 통해 사이드바의 모든 입력값 return 받기
-    def get_sidebar(self):
-        sb = Sidebar() 
-        return sb.result_sidebar()
-
+    def get_sidebar(self) : return self.sb.result_sidebar()
+        
     # FuncTion.py의 Function클래스의 return 받아 오기 // 추후 기능부로 수정하기
-    def get_function(self, data, blood, clst, hbit, gender, heart, age):
-        # 머신 러닝 <- 형변환
-        fc = Function(data, int(blood), int(clst), int(hbit), float(gender), int(heart), float(age))  # 객체 생성 타입 형변환
-        return fc.create_model()
+    def get_function(self, data, blood, clst, hbit, gender, heart, age) : return self.fc.create_model()
