fe9d697c4208e52df3e4009c756478c9aff4c9c7
cc5547 <126434132+cc5547@users.noreply.github.com>
2023-03-29 21:39:07 +0900

    go

--- submission/cc5547/03_third_webapp/Handle.py
+++ submission/cc5547/03_third_webapp/Handle.py
@@ -9,18 +9,19 @@ class Get:
         
     # joblib불러오기
     def get_data(self):
-        dt = CreateData()  # 객체 생성
+        dt = CreateData()
         data = dt.create_data()
         return data
 
     # SideBar.py의 Sidebar 클래스의 result_sidebar()를 통해 사이드바의 모든 입력값 return 받기
     def get_sidebar(self):
-        sb = Sidebar()  # 객체 생성
+        sb = Sidebar()
         result = sb.result_sidebar()
         return result
 
     # FuncTion.py의 Function클래스의 return 받아 오기 // 추후 기능부로 수정하기
     def get_function(self, data, blood, clst, hbit, gender, heart, age):
+        # 머신 러닝 <- 형변환
         fc = Function(data, int(blood), int(clst), int(hbit), float(gender), int(heart), float(age))  # 객체 생성 타입 형변환
         result = fc.create_model()
         return result
