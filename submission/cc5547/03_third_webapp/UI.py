9b020831ffe392af8f6a1a0095bca71333192674
cc5547 <126434132+cc5547@users.noreply.github.com>
2023-03-29 22:59:21 +0900

    go

--- submission/cc5547/03_third_webapp/UI.py
+++ submission/cc5547/03_third_webapp/UI.py
@@ -33,7 +33,7 @@ def user_interface(blood, clst, hbit, gender, heart, age) :
         elif tf == 1 : st.write("# 분석 결과 😊 <span style='color:blue'>정상</span> 😊입니다.", unsafe_allow_html=True)
         
         st.write(f"""
-            # 결과에 실망하지 마세요😭
-            # 👇확률을 알려드립니다👇
-            # 👉 {', '.join([f'{p*100:.4f}%' for p in tf_p])}
+            ## 결과에 실망하지 마세요😭
+            ### 👇확률을 알려드립니다👇
+            ### 👉 {', '.join([f'{p*100:.4f}%' for p in tf_p])}
         """)
