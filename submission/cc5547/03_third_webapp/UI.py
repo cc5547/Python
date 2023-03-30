52851fe49aea86bf84b2d8290b47e453c0a1a910
cc5547 <126434132+cc5547@users.noreply.github.com>
2023-03-30 16:50:12 +0900

    go

--- submission/cc5547/03_third_webapp/UI.py
+++ submission/cc5547/03_third_webapp/UI.py
@@ -15,10 +15,10 @@ def user_interface(blood, clst, hbit, gender, heart, age) :
 
     col1, col2 = st.columns([4, 6])
     with col1 :
-        if tf == 0 : st.write("""
+        if tf == 1 : st.write("""
                                 # 분석 결과 
                                 # 🤦‍♂️ <span style='color:red'>고혈압</span> 🤦‍♂️입니다. """, unsafe_allow_html=True)
-        elif tf == 1 : st.write("""
+        elif tf == 0 : st.write("""
                                 # 분석 결과 
                                 # 😊 <span style='color:blue'>정상</span> 😊입니다. """, unsafe_allow_html=True)
         else : pass
