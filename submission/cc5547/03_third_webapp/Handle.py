ba25e2eb251ef17ad001658bc8133e10ee62486b
cc5547 <126434132+cc5547@users.noreply.github.com>
2023-03-30 09:03:30 +0900

    go

--- submission/cc5547/03_third_webapp/Handle.py
+++ submission/cc5547/03_third_webapp/Handle.py
@@ -5,15 +5,15 @@ from FuncTion import Function
 # Data, FuncTion, SideBar Class 들의 return 값들을 다 받아 오는 클래스 
 class Get : # test
     def __init__(self) -> None:
-        pass
-        
+        self.dt = CreateData()
+        self.sb = Sidebar()
     # joblib불러오기
     def get_data(self) : 
-        return CreateData.result_data()
+        return self.dt.result_data()
 
     # SideBar.py의 Sidebar 클래스의 result_sidebar()를 통해 사이드바의 모든 입력값 return 받기
     def get_sidebar(self) : 
-        return Sidebar.result_sidebar()
+        return self.sb.result_sidebar()
 
     def get_function(self, data, blood, clst, hbit, gender, heart, age) :
         return Function(data, int(blood), int(clst), int(hbit), float(gender), int(heart), float(age)).result_model()
