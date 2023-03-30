87864c0bfe985f829d1528b5bb7e01e889d80c58
cc5547 <126434132+cc5547@users.noreply.github.com>
2023-03-30 15:05:39 +0900

    해

--- submission/cc5547/03_third_webapp/SideBar.py
+++ submission/cc5547/03_third_webapp/SideBar.py
@@ -30,7 +30,7 @@ class Sidebar:
         
     # 심장병 유무 int로 후에 형변환
     def heart_sick(self, hbit) : 
-        if hbit is not None and hbit > 1 : return 0 if self.s.radio('심장병이 있습니까?', self.y_n) == "예" else 1
+        if hbit is not None and hbit > 1 : return 1 if self.s.radio('심장병이 있습니까?', self.y_n) == "예" else 0
         else : return None
         
     # 나이 받기 float로 후에 형변환
