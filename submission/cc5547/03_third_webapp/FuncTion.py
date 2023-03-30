ce4450e7a77891349dd7d371032f51d792c0031d
cc5547 <126434132+cc5547@users.noreply.github.com>
2023-03-30 17:05:08 +0900

    go

--- submission/cc5547/03_third_webapp/FuncTion.py
+++ submission/cc5547/03_third_webapp/FuncTion.py
@@ -2,6 +2,8 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import plotly.express as px
+import plotly.graph_objects as go
 
 class Function:
     def __init__(self, data, blood, clst, hbit, gender, heart, age) -> None:
