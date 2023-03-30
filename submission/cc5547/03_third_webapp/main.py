5088453307f1d1b81ecd90933a23f109d07b1117
cc5547 <126434132+cc5547@users.noreply.github.com>
2023-03-31 08:53:36 +0900

    go

--- submission/cc5547/03_third_webapp/main.py
+++ submission/cc5547/03_third_webapp/main.py
@@ -1,11 +1,11 @@
 import streamlit as st
 import UI
-import Get
+import Handle
 img_url = '<a href="https://sparkly-prince-933.notion.site/1ccb865a95e54590bfd61e22b45520fa"><img src="https://i.imgur.com/ktulthH.gif" width=1200></a>'
 
 # main 시작점
 def main():
-    blood, clst, hbit, gender, heart, age = Get.get_sidebar()
+    blood, clst, hbit, gender, heart, age = Handle.get_sidebar()
     
     if age != "" :
         UI.user_interface(blood, clst, hbit, gender, heart, age)  
