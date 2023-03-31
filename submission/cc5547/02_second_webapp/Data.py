5b370e24d81a1c5bd64f2cbd63f593f576eae1f9
최용재 <126434132+cc5547@users.noreply.github.com>
2023-03-31 15:39:48 +0900

    go

--- submission/cc5547/02_second_webapp/Data.py
+++ submission/cc5547/02_second_webapp/Data.py
@@ -1,5 +1,16 @@
-import streamlit as st
+import pandas as pd
 
 class Create:
-    def __init__
+    def __init__(self) -> None :
+        self.df_URL = "submission/cc5547/02_second_webapp/%EC%8B%9C%ED%97%98%EC%9E%A5%EC%86%8C_%EA%B0%80%EA%B3%B5%EC%B2%98%EB%A6%AC.csv"
+        self.df_URL_g1 = "submission/cc5547/02_second_webapp/%EA%B7%B8%EB%9E%98%ED%94%84_1.csv"
+        self.df_URL_g2 = "submission/cc5547/02_second_webapp/%EA%B7%B8%EB%9E%98%ED%94%84_2.csv"
 
+    def create_df(self) :
+        self.df = pd.read_csv(self.df_URL).iloc[:, 1:].index += 1
+        self.df_g1 = pd.read_csv(self.df_URL_g1)
+        self.df_g2 = pd.read_csv(self.df_URL_g2)
+        return df, df_g1, df_g2
+
+    def get_data(self) :
+        return self.create_df()
--- submission/cc5547/02_second_webapp/app.py
+++ submission/cc5547/02_second_webapp/app.py
@@ -5,22 +5,7 @@ import pandas as pd
 import numpy as np
 import plotly.express as px
 import plotly.graph_objects as go
-
-st.set_page_config(layout="wide")
-
-# 데이터 프레임 생성
-def create_df():
-  df_URL = "https://raw.githubusercontent.com/cc5547/Python/main/submission/cc5547/02_second_webapp/%EC%8B%9C%ED%97%98%EC%9E%A5%EC%86%8C_%EA%B0%80%EA%B3%B5%EC%B2%98%EB%A6%AC.csv"
-  df_URL_g1 = "https://raw.githubusercontent.com/cc5547/Python/main/submission/cc5547/02_second_webapp/%EA%B7%B8%EB%9E%98%ED%94%84_1.csv"
-  df_URL_g2 = "https://raw.githubusercontent.com/cc5547/Python/main/submission/cc5547/02_second_webapp/%EA%B7%B8%EB%9E%98%ED%94%84_2.csv"
-
-  df = pd.read_csv(df_URL).iloc[:, 1:]
-  df_g1 = pd.read_csv(df_URL_g1)
-  df_g2 = pd.read_csv(df_URL_g2)
-  
-  df.index += 1
-
-  return df, df_g1, df_g2
+st.set_page_config(page_title="마싯는 머신러닝", layout="wide")
 
 # 사이드바
 def side_bar(df) :
