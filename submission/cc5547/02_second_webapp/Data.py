037d93a12c5af98d3e7a1df14fd9491d88b4febb
최용재 <126434132+cc5547@users.noreply.github.com>
2023-03-31 17:47:41 +0900

    go

--- submission/cc5547/02_second_webapp/Data.py
+++ submission/cc5547/02_second_webapp/Data.py
@@ -2,17 +2,16 @@ import pandas as pd
 
 class Create:
     def __init__(self) -> None :
-        # self.df_URL = "https://github.com/cc5547/Python/blob/main/submission/cc5547/02_second_webapp/%EC%8B%9C%ED%97%98%EC%9E%A5%EC%86%8C_%EA%B0%80%EA%B3%B5%EC%B2%98%EB%A6%AC.csv"
-        # self.df_URL_g1 = "https://raw.githubusercontent.com/cc5547/Python/main/submission/cc5547/02_second_webapp/%EA%B7%B8%EB%9E%98%ED%94%84_1.csv"
-        # self.df_URL_g2 = "https://raw.githubusercontent.com/cc5547/Python/main/submission/cc5547/02_second_webapp/%EA%B7%B8%EB%9E%98%ED%94%84_2.csv"
-        pass
-    # def create_data(self) :
-    #     df = pd.read_csv(self.df_URL).iloc[:, 1:]
-    #     df.index += 1
-    #     df_g1 = pd.read_csv(self.df_URL_g1)
-    #     df_g2 = pd.read_csv(self.df_URL_g2)
-    #     return df, df_g1, df_g2
+        self.df_URL = "https://github.com/cc5547/Python/blob/main/submission/cc5547/02_second_webapp/%EC%8B%9C%ED%97%98%EC%9E%A5%EC%86%8C_%EA%B0%80%EA%B3%B5%EC%B2%98%EB%A6%AC.csv"
+        self.df_URL_g1 = "https://raw.githubusercontent.com/cc5547/Python/main/submission/cc5547/02_second_webapp/%EA%B7%B8%EB%9E%98%ED%94%84_1.csv"
+        self.df_URL_g2 = "https://raw.githubusercontent.com/cc5547/Python/main/submission/cc5547/02_second_webapp/%EA%B7%B8%EB%9E%98%ED%94%84_2.csv"
+        
     def create_data(self) :
-        return 1
+        df = pd.read_csv(self.df_URL).iloc[:, 1:]
+        df.index += 1
+        df_g1 = pd.read_csv(self.df_URL_g1)
+        df_g2 = pd.read_csv(self.df_URL_g2)
+        return df, df_g1, df_g2
+
     def get_data(self) :
         return self.create_data()
