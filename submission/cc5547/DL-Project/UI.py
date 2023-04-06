f1467a49292830e2d58ce631e24f0be9501113d0
최용재 <126434132+cc5547@users.noreply.github.com>
2023-04-06 12:12:15 +0900

    go

--- submission/cc5547/DL-Project/UI.py
+++ submission/cc5547/DL-Project/UI.py
@@ -8,8 +8,6 @@ def user_interface():
     containers = [st.container() for i in range(container_count)]
     image = ["https://i.imgur.com/t4O7ozH.jpg", "https://i.imgur.com/idnsDBs.gif", "https://i.imgur.com/fvRG1Tj.gif"]
 
-    [st.image(image, width=500) for image in (image_list = [image for image in image_urls])]
-
-    # for i in range(container_count):
-    #     with containers[i]:
-    #         st.image(image[i], width = 500)
+    for i in range(container_count):
+        with containers[i]:
+            st.image(image[i], width = 500)
