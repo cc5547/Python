9be96e1a5d74b28332c6afd6751ccd4774d15115
최용재 <126434132+cc5547@users.noreply.github.com>
2023-04-06 12:08:03 +0900

    go

--- submission/cc5547/DL-Project/UI.py
+++ submission/cc5547/DL-Project/UI.py
@@ -4,34 +4,10 @@ st.set_page_config(page_title="마싯는 머신러닝", layout="wide")
 def user_interface():
     st.title("뼈대 작업 중...")
 
-    container_count = 10
+    container_count = 3
     containers = [st.container() for i in range(container_count)]
-    with containers[0] :
-        st.image("https://i.imgur.com/t4O7ozH.jpg", width = 500)
-        
-    with containers[1] :
-        st.image("https://i.imgur.com/idnsDBs.gif", width = 500)
+    image = ["https://i.imgur.com/t4O7ozH.jpg", "https://i.imgur.com/idnsDBs.gif", "https://i.imgur.com/fvRG1Tj.gif"]
 
-    with containers[2] :
-        st.image("https://i.imgur.com/fvRG1Tj.gif", width = 500)
-
-    with containers[3] :
-        pass
-
-    with containers[4] :
-        pass
-
-    with containers[5] :
-        pass
-
-    with containers[6] :
-        pass
-
-    with containers[7] :
-        pass
-
-    with containers[8] :
-        pass
-        
-    with containers[9] :
-        pass
+    for i in range(container_count):
+        with containers[i]:
+            st.image(image[i], width = 500)
