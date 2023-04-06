117afbb560ccd41f4a47778eb651ffdc89eaac8d
최용재 <126434132+cc5547@users.noreply.github.com>
2023-04-06 12:00:29 +0900

    go

--- submission/cc5547/DL-Project/UI.py
+++ submission/cc5547/DL-Project/UI.py
@@ -4,7 +4,8 @@ st.set_page_config(page_title="마싯는 머신러닝", layout="wide")
 def user_interface():
     st.title("뼈대 작업 중...")
 
-    containers = [st.container() for i in range(3)]
+    container_count = 3
+    containers = [st.container() for i in range(container_count)]
     with containers[0]:
         st.image("https://i.imgur.com/t4O7ozH.jpg", width = 500)
         
