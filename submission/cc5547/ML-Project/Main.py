c44aaae0ba90d2d1a1c580b8e245780ab615a378
최용재 <126434132+cc5547@users.noreply.github.com>
2023-04-06 10:59:32 +0900

    go

--- submission/cc5547/ML-Project/Main.py
+++ submission/cc5547/ML-Project/Main.py
@@ -1,7 +1,5 @@
 import UI
 
-st.set_page_config(page_title="마싯는 머신러닝", layout="wide")
-
 # main 시작점
 def main():
    UI.user_interface()
--- submission/cc5547/ML-Project/UI.py
+++ submission/cc5547/ML-Project/UI.py
@@ -1,4 +1,14 @@
 import streamlit as st
+st.set_page_config(page_title="마싯는 머신러닝", layout="wide")
 
 def user_interface():
-    pass
+    containers = [st.container() for i in range(3)]
+
+    with containers[0]:
+        st.image("https://i.imgur.com/fvRG1Tj.gif", width = 1000)
+        
+    with containers[1]:
+        st.image("https://i.imgur.com/idnsDBs.gif", width = 1000)
+
+    with containers[2]:
+        st.title("뼈대 작업 중...")
