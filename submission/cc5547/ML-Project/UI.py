00bbe2ea7858d513862974407b3ed2c7013725b9
최용재 <126434132+cc5547@users.noreply.github.com>
2023-04-06 11:13:02 +0900

    go

--- submission/cc5547/ML-Project/UI.py
+++ submission/cc5547/ML-Project/UI.py
@@ -3,7 +3,7 @@ st.set_page_config(page_title="마싯는 머신러닝", layout="wide")
 
 def user_interface():
     st.title("뼈대 작업 중...")
-    col1, col2 = st.columns([8, 2])
+    col1, col2 = st.columns([5, 5])
     with col1 :
         st.image("https://i.imgur.com/fvRG1Tj.gif", width = 500)
     with col2 :
